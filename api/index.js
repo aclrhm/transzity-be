9e3d61cdd47249354c015adc71b9a201ba362cb3
Azka Auliarahma <azkaauliarahma08@gmail.com>
2025-09-24 00:09:43 +0700

    local

--- api/index.js
+++ api/index.js
@@ -12,4 +12,11 @@ app.get("/", (req, res) => {
   res.send("Backend Transzity API is running on Vercel 🚀");
 });
 
+if(process.env.NODE_ENV !== 'production') {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
+
 export default app;
